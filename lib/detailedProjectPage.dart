e4fa3dca3c9b4eee7f087b4236ab0c7d1130fe0e
MehmetSakirSeker <mehmet.seker@std.izmirekonomi.edu.tr>
2025-05-10 11:59:40 +0300

    Project Detaile Page Update
    Now the state of the project and comments about the projects can be made and seen by others

---
+++ lib/detailedProjectPage.dart
@@ -0,0 +1,204 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Firebase Auth importu
+import 'package:flutter/material.dart';
+
+import 'auth/auth_service.dart';
+
+class DetailedProjectPage extends StatefulWidget {
+  final String title;
+  final String? previousState;
+
+
+
+  DetailedProjectPage({required this.title,this.previousState});
+
+  @override
+  _DetailedProjectPageState createState() => _DetailedProjectPageState();
+}
+
+class _DetailedProjectPageState extends State<DetailedProjectPage> {
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _assignedController = TextEditingController();
+  final TextEditingController _commentController = TextEditingController();
+  String? _selectedState;
+  String? _username;
+
+  final AuthService _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    // Kullanıcıyı FirebaseAuth ile alıyoruz
+    User? user = _authService.getCurrentUser();
+    _username = user?.email ?? 'Anonim'; // Kullanıcı adı alınır
+
+  }
+
+  Future<void> _addComment(String comment, String username) async {
+
+    if (comment.trim().isEmpty || username.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Comment Place cannot be null.')),
+      );
+      return;
+    }
+
+    try {
+      await FirebaseFirestore.instance.collection('comments').add({
+        'projectTitle': widget.title,
+        'comment': comment,
+        'username': username,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      _commentController.clear();
+    } catch (e) {
+      print('Adding comment error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error while sending message.')),
+      );
+    }
+  }
+  // Save butonuna tıklanınca proje state'ini güncelleyen fonksiyon
+  Future<void> _updateProjectState() async {
+    if (_selectedState == null && widget.previousState != null) {
+        _selectedState = widget.previousState;
+    }
+
+    try {
+      // Proje durumunu Firestore'da güncelle
+      await FirebaseFirestore.instance
+          .collection('projects')
+          .where('projectName', isEqualTo: widget.title)
+          .get()
+          .then((snapshot) {
+        for (var doc in snapshot.docs) {
+          doc.reference.update({'projectType': _selectedState});
+        }
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Project saved successfully.')),
+      );
+      Navigator.of(context).pop(_selectedState);// Dialogu kapat
+    } catch (e) {
+      print('Error updating project state: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error updating project state.')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(widget.title),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            SizedBox(height: 10),
+            DropdownButton<String>(
+              isExpanded: true,
+              value: _selectedState,
+              hint: Text('Set State'),
+              items: ['To Do', 'In Progress', 'Done', 'Bugs']
+                  .map((String value) => DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedState = value;
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _assignedController,
+              decoration: InputDecoration(labelText: 'Assigned'),
+            ),
+            Divider(height: 30),
+            // Kullanıcı adını sadece Text olarak gösteriyoruz
+            Text(
+              'Your Name: $_username', // Kullanıcının adı burada gösterilir
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              controller: _commentController,
+              decoration: InputDecoration(labelText: 'Add Comment'),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () async {
+                  await _addComment(
+                    _commentController.text,
+                    _username!,
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('comments')
+                  .where('projectTitle', isEqualTo: widget.title)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  return CircularProgressIndicator();
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty)
+                  return Text("No comments have been made.");
+
+                return Column(
+                  children: snapshot.data!.docs.map((doc) {
+                    final data = doc.data() as Map<String, dynamic>;
+                    final timestamp = data['timestamp'] as Timestamp?;
+                    final formattedTime = timestamp != null
+                        ? DateTime.fromMillisecondsSinceEpoch(
+                        timestamp.millisecondsSinceEpoch)
+                        .toLocal()
+                        .toString()
+                        .substring(0, 16) // yyyy-MM-dd HH:mm
+                        : 'Unknown time';
+                    return Column(
+                      children: [
+                        ListTile(
+                          contentPadding: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
+                          title: Text(data['username'] ?? 'No such user'),
+                          subtitle: Text('${data['comment'] ?? ''}\n$formattedTime'),
+                        ),
+                        Divider(), // Burada Divider ekliyoruz
+                      ],
+                    );
+
+                  }).toList(),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // Save butonuna basıldığında proje state'ini güncelle
+            _updateProjectState();
+          },
+          child: Text('Save'),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/mainpage/ProjectsController.dart
+++ lib/pages/mainpage/ProjectsController.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:jjj/detailedProjectPage.dart';
 
 //Object for saving to db
 class Project {
@@ -27,20 +28,22 @@ class _ProjectControllerState extends State<ProjectController> {
   }
 
   Future<void> _fetchTasksFromFirebase() async {
-    final snapshot = await FirebaseFirestore.instance
-        .collection('projects')
-        .orderBy('createdAt', descending: true)
-        .get();
+    final snapshot =
+        await FirebaseFirestore.instance
+            .collection('projects')
+            .orderBy('createdAt', descending: true)
+            .get();
 
     print('Fetched ${snapshot.docs.length} tasks from Firestore');
 
     setState(() {
-      projects = snapshot.docs.map((doc) {
-        return Project(
-          projectName: doc['projectName'],
-          projectType: doc['projectType'],
-        );
-      }).toList();
+      projects =
+          snapshot.docs.map((doc) {
+            return Project(
+              projectName: doc['projectName'],
+              projectType: doc['projectType'],
+            );
+          }).toList();
     });
   }
 
@@ -72,12 +75,15 @@ class _ProjectControllerState extends State<ProjectController> {
                   labelText: 'Project Type',
                   border: OutlineInputBorder(),
                 ),
-                items: ['To Do', 'In Progress', 'Completed', 'Bugs']
-                    .map((status) => DropdownMenuItem(
-                  value: status,
-                  child: Text(status),
-                ))
-                    .toList(),
+                items:
+                    ['To Do', 'In Progress', 'Completed', 'Bugs']
+                        .map(
+                          (status) => DropdownMenuItem(
+                            value: status,
+                            child: Text(status),
+                          ),
+                        )
+                        .toList(),
                 onChanged: (value) {
                   newTask.projectType = value;
                 },
@@ -93,7 +99,8 @@ class _ProjectControllerState extends State<ProjectController> {
             ),
             ElevatedButton(
               onPressed: () async {
-                if (newTask.projectName != null && newTask.projectType != null) {
+                if (newTask.projectName != null &&
+                    newTask.projectType != null) {
                   await _addTask(newTask);
                   Navigator.pop(context);
                 }
@@ -124,63 +131,89 @@ class _ProjectControllerState extends State<ProjectController> {
               child: ListView.builder(
                 padding: EdgeInsets.zero,
                 itemCount: projects.length,
-              itemBuilder: (context, index) {
-                final task = projects[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 5.0, horizontal: 16),
-                  child: Container(
-                    width: double.infinity,
-                    height: 80,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300]!,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.black54,
-                          child: Text(
-                            '${index + 1}',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          child: Text(
-                            task.projectName ?? '',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87,
+                itemBuilder: (context, index) {
+                  final task = projects[index];
+
+                  return GestureDetector(
+                    onTap: () async {
+                      final updatedState =await
+                      showDialog<String>(
+                        context: context,
+                        builder:
+                            (context) => DetailedProjectPage(
+                              title: task.projectName ?? "",
+                              previousState: task.projectType,
                             ),
-                          ),
+                      );
+                      if (updatedState != null) {
+                        setState(() {
+                          projects[index].projectType = updatedState;
+                        });
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5.0,
+                        horizontal: 16,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        height: 80,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300]!,
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                          decoration: BoxDecoration(
-                            color: Colors.black54,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Text(
-                            task.projectType ?? '',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.black54,
+                              child: Text(
+                                '${index + 1}',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                          ),
+                            SizedBox(width: 16),
+                            Expanded(
+                              child: Text(
+                                task.projectName ?? '',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 6,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                task.projectType ?? '',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
-        )],
+        ],
       ),
     );
   }
